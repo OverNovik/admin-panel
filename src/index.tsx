bac430c31cb3e5b2874f0fd78dcc3ad813b23d84
Aliaxander Novikov <nerevarin11@gmail.com>
2022-06-11 14:01:27 +0300

    add apollo cli, graphql

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,22 @@ import ReactDOM from "react-dom/client";
 import 'antd/dist/antd.min.css';
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const client = new ApolloClient({
+  cache: new InMemoryCache,
+  uri: 'https://graphqlzero.almansi.me/api',
+})
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
