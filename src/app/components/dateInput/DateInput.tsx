cf42686105a2b8ea868f0042a89301ebce98e3a7
Aliaxander Novikov <nerevarin11@gmail.com>
2022-06-17 12:17:37 +0300

    feat: add DateInput

---
+++ src/app/components/dateInput/DateInput.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { DatePicker } from "antd";
+import type { Moment } from "moment";
+
+const { RangePicker } = DatePicker;
+
+type RangeValue = [Moment | null, Moment | null] | null;
+
+const DateInput: React.FC = () => {
+  const [dates, setDates] = useState<RangeValue>(null);
+  const [hackValue, setHackValue] = useState<RangeValue>(null);
+  const [value, setValue] = useState<RangeValue>(null);
+
+  const disabledDate = (current: Moment) => {
+    if (!dates) {
+      return false;
+    }
+    const tooLate = dates[0] && current.diff(dates[0], "days") > 7;
+    const tooEarly = dates[1] && dates[1].diff(current, "days") > 7;
+    return !!tooEarly || !!tooLate;
+  };
+
+  const onOpenChange = (open: boolean) => {
+    if (open) {
+      setHackValue([null, null]);
+      setDates([null, null]);
+    } else {
+      setHackValue(null);
+    }
+  };
+
+  return (
+    <RangePicker
+      value={hackValue || value}
+      disabledDate={disabledDate}
+      onCalendarChange={(val) => setDates(val)}
+      onChange={(val) => setValue(val)}
+      onOpenChange={onOpenChange}
+    />
+  );
+};
+
+export default DateInput;
--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { Button, Layout, Typography } from "antd";
+import React, { useState } from "react";
+import { CalendarOutlined } from "@ant-design/icons";
+import { Button, Dropdown, Layout, Typography } from "antd";
 import { useAuth } from "app/utils";
+import { DateInput } from "..";
 import styles from "./style.module.css";
 
 const { Header: HeaderEl } = Layout;
@@ -8,6 +10,7 @@ const { Title } = Typography;
 
 const Header: React.FC = () => {
   const auth: any = useAuth();
+  const [isVisible, setVisible] = useState(false);
 
   return (
     <HeaderEl className={styles.header}>
@@ -15,9 +18,21 @@ const Header: React.FC = () => {
         Admin-panel
       </Title>
       {auth.loggedIn ? (
-        <Button danger className={styles.logout} onClick={auth.logOut}>
-          LogOut
-        </Button>
+        <>
+          <Dropdown
+            placement="bottom"
+            visible={isVisible}
+            arrow
+            overlay={<DateInput />}
+          >
+            <Button type="primary" onClick={() => setVisible(!isVisible)}>
+              <CalendarOutlined />
+            </Button>
+          </Dropdown>
+          <Button danger className={styles.logout} onClick={auth.logOut}>
+            LogOut
+          </Button>
+        </>
       ) : null}
     </HeaderEl>
   );
