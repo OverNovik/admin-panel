ec7987ab35c62c975eb8713742b17588e01fb39f
Aliaxander Novikov <nerevarin11@gmail.com>
2022-06-23 02:44:55 +0300

    fix: fix type error #2

--- src/components/dateInput/DateInput.tsx
+++ src/components/dateInput/DateInput.tsx
@@ -3,28 +3,20 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const DateInput: React.FC = () => {
-  const [startDate, setStartDate] = useState<
-    React.SetStateAction<Date | undefined>
-  >(new Date());
-  const [endDate, setEndDate] = useState<
-    React.SetStateAction<Date | undefined>
-  >(new Date());
+  const [startDate, setStartDate] = useState<Date | undefined>(new Date());
+  const [endDate, setEndDate] = useState<Date | undefined>(new Date());
   return (
     <>
       <DatePicker
         selected={startDate}
-        onChange={(date: React.SetStateAction<Date | undefined>) =>
-          setStartDate(date)
-        }
+        onChange={(date: Date | undefined) => setStartDate(date)}
         selectsStart
         startDate={startDate}
         endDate={endDate}
       />
       <DatePicker
         selected={endDate}
-        onChange={(date: React.SetStateAction<Date | undefined>) =>
-          setEndDate(date)
-        }
+        onChange={(date: Date | undefined) => setEndDate(date)}
         selectsEnd
         startDate={startDate}
         endDate={endDate}
